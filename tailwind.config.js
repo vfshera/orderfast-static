e743be19153d2e30474edc201ba4e876278f34af
Franklin Shera <fshera96@gmail.com>
2022-11-30 15:11:12 +0300

    from dynamic

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#212121",
+
+        purple: "#ce2ee2",
+        lavendar: "#f7d8fa",
+      },
+    },
+  },
+  plugins: [],
+};
